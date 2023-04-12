89f791917a41c892afab996184d8efa58b79e25b
Roop <roop9540@gmail.com>
2023-04-13 00:20:47 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // let [email, setEmail] = useState("");
+    
+    let [name, setName] = useState("");
+    let [username, setusername] = useState("");
+    let [password, setpassword] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log({ name: name, username: username, password: password })
+        try {
+            const data = await axios.post(process.env.REACT_APP_API_BASE_URL+'register', { name: name, username: username, password: password })
+            if (data.status === 200) {
+
+                alert("post successfully")
+            }
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+    return (
+        <>
+            <div className='home-bg vh-100' >
+                <div className='container' >
+                    <h1 className='text-center' >Register Form</h1>
+
+                    <form className='d-grid gap-2 col-8 mx-auto mt-3'  onSubmit={handleSubmit} >
+                        <div className="  mb-3">
+                            <label htmlfor="exampleInputName" className="form-label">Name</label>
+                            <input type="name" className="form-control" onChange={(e) => { setName(e.target.value) }} id="exampleInputName" aria-describedby="nameHelp" />
+
+                        </div>
+                        <div className="  mb-3">
+                            <label htmlfor="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="username" className="form-control" onChange={(e) => { setusername(e.target.value) }} id="exampleInputEmail1" aria-describedby="emailHelp" />
+
+                        </div>
+                        <div className="mb-3">
+                            <label htmlfor="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" className="form-control" onChange={(e) => { setpassword(e.target.value) }} id="exampleInputPassword1" />
+                        </div>
+                        <div className='d-grid gap-2 col-10 mx-auto ' >
+                            <button type="submit" className="btn btn-primary rounded-pill mt-3">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App;
